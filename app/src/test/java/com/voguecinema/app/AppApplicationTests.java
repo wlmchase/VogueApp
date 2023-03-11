69380855df5182d14ababfb5b06e42c65ec1754d
mackenzieChase <wlmchase@gmail.com>
2023-03-11 09:53:32 -0400

    initial commit

---
+++ app/src/test/java/com/voguecinema/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voguecinema.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
